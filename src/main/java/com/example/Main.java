dd867fb038730fd6c4ee273bd0eeb562f3d55391
vijayadroit-ctr <avvijay.adroittechnologies@gmail.com>
2026-03-31 10:01:54 +0530

    Files saved into github

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        for(int k=0;k<5;k++){
+            System.out.print("Hello World ");
+            System.out.println(k+1);
+            Thread.sleep(2000);
+        }
+    }
+}
